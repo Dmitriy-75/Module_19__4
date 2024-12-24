fa67500eac819a93e060e8f609899cbf73e21dc4
Dmitriy-75 <dimakasimov29@gmail.com>
2024-12-24 08:45:24 +0300

    Add files via upload
    Домашнее задание по теме "Сайт администрирования"
    
    Ссылка на предыдущую версию дз.
    
    В этом домашнем задании Вам предстоит добавить Ваши модели Game и Buyer, созданные в предыдущих заданиях, в админку.
    Добавьте в файл admin.py Вашего приложения следующие классы:
    GameAdmin - админ-класс модели Game, в него добавьте:
    Фильтрацию по полям size и cost.
    Отображение полей title, cost и size при отображении всех полей списком.
    Поиск по полю title.
    Ограничение кол-ва записей до 20.
    GameAdmin - админ-класс модели Game, в него добавьте:
    Фильтрацию по полям balance и age.
    Отображение полей name, balance и age при отображении всех полей списком.
    Поиск по полю name.
    Ограничение кол-ва записей до 30.
    Доступным только для чтения поле balance.
    *Не забудьте наследовать ваши админ-классы от ModelAdmin и добавить к ним декоратор @admin.register.

---
+++ ShopDj/task1/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+
+# Создайте в приложении task1 следующие модели:
+#
+# 1.Buyer - модель представляющая покупателя.
+# Обладает следующими полями:
+# name - имя покупателя(username аккаунта)
+# balance - баланс(DecimalField)
+# age - возраст.
+#
+# 2.Game - модель представляющая игру.
+# Обладает следующими полями:
+# title - название игры
+# cost - цена(DecimalField)
+# size - размер файлов игры(DecimalField)
+# description - описание(неограниченное кол-во текста)
+# age_limited - ограничение возраста 18+ (BooleanField, по умолчанию False)
+# buyer - покупатель обладающий игрой (ManyToManyField). У каждого покупателя может быть игра
+#           и у каждой игры может быть несколько обладателей.
+# DecimalField - поле для дробных чисел.
+# BooleanField - поле для булевых значений.
+
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=15)
+    balance = models.DecimalField(max_digits=6, decimal_places=2)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length = 15)
+    cost = models.DecimalField(max_digits=6,decimal_places=2)
+    size = models.DecimalField(max_digits=4,decimal_places=2)
+    description = models.TextField(blank=True)
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='games')
+
+    def __str__(self):
+        return self.title
+
